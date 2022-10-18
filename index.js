7cff9a3f37627c965cb08f47c9ca48e674515f92
theobassan <theobassan@gmail.com>
2022-10-18 11:46:12 -0300

    nre vercel config

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const app = require('express')();
 const {Noco} = require("nocodb");
+
 const httpServer = app.listen(process.env.PORT || 8080);
 app.use(await Noco.init({}, httpServer, app));
+
+module.exports = app
